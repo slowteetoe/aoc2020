2cb4af3cef817c627aa9d780118c57eb38b789ac
Steven Lotito <steven.lotito@gmail.com>
2023-06-24 08:36:40 -0700

    fix test

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -65,6 +65,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(241861950));
     }
 }
